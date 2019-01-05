c7139cf327c99edf82d9df6106e5da98d3671a3a
mmfly <627162322@qq.com>
2019-01-05 12:10:52 +0800

    init

---
+++ src/main/java/com/mmflys/datastructure/Main.java
@@ -0,0 +1,14 @@
+package com.mmflys.datastructure;
+
+/**
+ * @Author: Shark Chili
+ * @Email: sharkchili.su@gmail.com
+ * @Date: 2019/1/5 0005
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/QuickTest.java
@@ -0,0 +1,7 @@
+/**
+ * @Author: Shark Chili
+ * @Email: sharkchili.su@gmail.com
+ * @Date: 2019/1/5 0005
+ */
+public class QuickTest {
+}
